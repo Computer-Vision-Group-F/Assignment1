482bfda0f4cb85bf2c8aa92a4f5455607d624a22
Aman Singh <singh.aman167@gmail.com>
2024-10-10 02:29:27 -0400

    Initial commit for the knn.py updating gitignore

---
+++ knn.py
@@ -0,0 +1,49 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import data_utils
+import download
+
+url = "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
+download_dir = "./data"
+download.maybe_download_and_extract(url,download_dir)
+
+cifar10_dir = './data/cifar-10-batches-py'
+X_train, y_train, X_test, y_test = data_utils.load_CIFAR10(cifar10_dir)
+
+# Checking the size of the training and testing data
+print('Training data shape: ', X_train.shape)
+print('Training labels shape: ', y_train.shape)
+print('Test data shape: ', X_test.shape)
+print('Test labels shape: ', y_test.shape)
+
+classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+num_classes = len(classes)
+samples_per_class = 7
+for y, cls in enumerate(classes):
+    idxs = np.flatnonzero(y_train == y)
+    idxs = np.random.choice(idxs, samples_per_class, replace=False)
+    for i, idx in enumerate(idxs):
+        plt_idx = i * num_classes + y + 1
+        plt.subplot(samples_per_class, num_classes, plt_idx)
+        plt.imshow(X_train[idx].astype('uint8'))
+        plt.axis('off')
+        if i == 0:
+            plt.title(cls)
+plt.show()
+
+# Memory error prevention by subsampling data
+
+num_training = 500
+mask = list(range(num_training))
+X_train = X_train[mask]
+y_train = y_train[mask]
+
+num_test = 250
+mask = list(range(num_test))
+X_test = X_test[mask]
+y_test = y_test[mask]
+
+# reshaping data and placing into rows
+X_train = np.reshape(X_train, (X_train.shape[0], -1))
+X_test = np.reshape(X_test, (X_test.shape[0], -1))
+print(X_train.shape, X_test.shape)
